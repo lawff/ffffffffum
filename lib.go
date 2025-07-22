30f763a0e96546a64c826a65c11a4a82d834c87e
kira <evilllawliet@gmail.com>
2025-07-22 10:09:34 +0800

    fix: ad doc

--- lib.go
+++ lib.go
@@ -1,5 +1,7 @@
 package ffffffffum
 
+// Add is a function that takes in two integer values and returns their sum.
+// It follows the rules specified by https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
